f90a47239179390a94204db567b1da5abfdbd5c5
Steve Owens <stevejowens@icloud.com>
2020-09-21 15:03:15 +0100

    Add visual Studio 2019 Extensions

--- devBoxSetup.ps1
+++ devBoxSetup.ps1
@@ -1,3 +1,28 @@
+
+
+
+
+function InstallVSExtension 
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string] $ExtensionName,
+        [Parameter(Mandatory=$true)]
+        [string] $FileName,
+        [Parameter(Mandatory=$true)]
+        [string] $DownloadURL
+    )
+
+    Process
+    {
+        $dl = (Get-Location).Path
+        Invoke-WebRequest -Uri $DownloadURL -OutFile "$dl/$FileName"
+        VSIXInstaller.exe /quiet $dl/$$FileName
+        Write-Host("Visual Studio 2019 Extension - $ExtensionName, Installed Successfully")
+    }
+}
+
 # Install Chocolatey Packages
 choco install git -y
 choco install vsts-cli -y
@@ -13,5 +38,16 @@ choco install service-fabric-sdk  -y
 choco install visualstudio2019professional --package-parameters "--add Microsoft.VisualStudio.Workload.CoreEditor --add Microsoft.VisualStudio.Workload.Data --add Microsoft.VisualStudio.Workload.DataScience --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetCoreTools --add Microsoft.VisualStudio.Workload.NetCrossPlat --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.Universal --add Microsoft.VisualStudio.Workload.Azure --includeRecommended --includeOptional --passive --locale en-US" -y
 choco install postman -y
 
-# Install VS Code Extentions
+# Install VS Code Extensions
 code --install-extension extensions.txt
+
+# Install Visual Studio 2019 Extensions
+
+
+$VSES = (Get-Content vs_extensions.json| ConvertFrom-Json)
+
+foreach ($VSE in $VSES)
+{
+    InstallVSExtension -ExtensionName $VSE.ExtensionName -FileName $VSE.FileName -DownloadURL $VSE.DownloadURL
+}
+
