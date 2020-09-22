3b49d2921409567fef492bcde4dcee0b840a982d
steve owens <steve.owens@fujitsu.com>
2020-09-22 11:03:59 +0100

    Add error checking to script

--- devBoxSetup.ps1
+++ devBoxSetup.ps1
@@ -1,15 +1,39 @@
 #Requires -Version 3.0
 <#
     .DESCRIPTION
+        This script will install Choco onto a Windows OS if it is not already installed.
         This script will install Chocolatey packages, VSCode Extensions and Visual Studio 2019 Extensions onto a Windows OS.
-        The script must be run with elevated admin permissions for PowerShell.
+        Script MUST be executed in a PowerShell session running as Administrator.
         The script must be run in the same directory as the configuration files, see GIT Repo readme for more details.
+        
     
     .NOTES
         AUTHOR: Steve Owens
         LASTEDIT: Sep 21, 2020
         VERSION: 1.0.0.1
+        
 #>
+
+# Validate that the script is running as Administrator.
+$isEvlevated = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544");
+if (!$isEvlevated) {
+        throw "Script needs to be run as an administrator";
+}
+
+if (!(Test-Path "$($env:ProgramData)\chocolatey\choco.exe")) {
+    Write-Host("installing chocolatey")
+    try {
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+    }
+    catch {
+        Write-Host($_.Exception.Message)
+    }
+}
+else {
+    Write-Host("chocolatey is already installed")
+}
 
 function InstallVSExtension 
 {
@@ -33,16 +57,16 @@ function InstallVSExtension
 }
 
 # Install Chocolatey Packages
-choco install packages.config -y
+#choco install packages.config -y
 
 # Install VS Code Extensions
-code --install-extension vscode_extensions.txt
+#code --install-extension vscode_extensions.txt
 
 # Install Visual Studio 2019 Extensions
-$VSES = (Get-Content vs_extensions.json| ConvertFrom-Json)
+#$VSES = (Get-Content vs_extensions.json| ConvertFrom-Json)
 
-foreach ($VSE in $VSES)
-{
-    InstallVSExtension -ExtensionName $VSE.ExtensionName -FileName $VSE.FileName -DownloadURL $VSE.DownloadURL
-}
+#foreach ($VSE in $VSES)
+#{
+#    InstallVSExtension -ExtensionName $VSE.ExtensionName -FileName $VSE.FileName -DownloadURL $VSE.DownloadURL
+#}
 
--- readme.md
+++ readme.md
@@ -1,13 +1,8 @@
 # DotNetCore and ServiceFabric Dev Book Setup
 
-A set of scripts to be run on a Window System to install and configure a DotNetCore Developer box with Additional features such as ServiceFabric and SpecFlow.
+A set of script to be run on a Window System to install and configure a DotNetCore Developer box with Additional features such as ServiceFabric and SpecFlow.
 
-## Install Choco
-Run the following PowerShell Script on a Windows to install choco, must be run as admin.
-
-Powershell Command: ./chocoSetup.ps1
-
-## Package Configuratio and Setup
+## Package Configuration and Setup
 
 packages.config file is used for the packages that are required to be installed by Choco/Chocolatey, amend as required.
 
@@ -15,7 +10,7 @@ vs_extensions.json file is used for the Visual Studio 2019 marketplace extension
 
 vscode_requirements.txt file is used for the Visual Studio Code marketplace extensions that are required to be installed by Visual Studio Code Cli, amend as required. Get the download URL and FileName for Visual Studio Code Extension Marketplace.
 
-## Build Out the Environment (If Chocolatey Already Installed)
+## Build Out the Environment
 Run the following PowerShell Script on a Windows OS using an Administrative permissions.
 
 Powershell Command: ./devBoxSetup.ps1
